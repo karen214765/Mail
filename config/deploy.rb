b5753c67a4d9a2acf0941fb6f0de1358f064e8fb
Karen <kvilla@uach.mx>
2011-10-18 11:09:24 -0500

    Se modifico el archivo deploy con mi configuracion

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "Mail"
+set :repository,  "git@github.com:karen214765/Mail.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :domain,"labs.ti.uach.mx"
+set :user, 'karen'
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+
+role :web, domain                         # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+  task :bundle_gems do
+    run "cd #{deploy_to}/current && bundle install vendor/gems"
+  end
+
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
